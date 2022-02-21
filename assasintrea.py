3bb213af539aad5cf896922425f127e6fa9a99c8
Mr.DDOS ATTACK <96361084+Xsarjame@users.noreply.github.com>
2022-02-21 12:06:33 +0700

    Create assasintrea.py

--- assasintrea.py
+++ assasintrea.py
@@ -3402,10 +3402,61 @@ def buildblock(size):
 
 def usage():
 	print '---------------------------------------------------'
-	print 'USAGE: python hulkV2.py <url>'
-	print 'you can add "safe" after url, to autoshut after dos'
+	print 'USAGE: python2 assasintrea.py <url>'
+	print 'you can add "safe" after url, to autoshut after ddos'
 	print '---------------------------------------------------'
 
+print \
+"""
+                  /\    
+                 / |\   
+                / /\ \   
+               / /  \ \  
+              / /    \ |
+             /_/      \_|
+             \    '`    /
+              )   ||   ( 
+              |   ||   | 
+              |   ||   | 
+              |   ||   |
+              |   ||   |
+              |   ||   | 
+              |   ||   | 
+              |   ||   | 
+              |   ||   | 
+              |   ||   |
+  /           |   ||   |           =
+ /(           |   ||   |           )=
+ |`\_         |   ||   |         _/'|
+ |`. `-._     |   ||   |     _,-' ,'|
+ (   ` . `-._ |  _--_  | _,-' , '   )
+  `.._   ` . `-./.__.\.-' , '   _,-'
+      `-._   ` | /  \ | '   _,-'
+          `-._/ |_()_| \_,-'
+       ___.-'   ______   `-,
+      '-----.  |______|   /  I'm an assassin I was 
+             \  ______   /   in fear and exhaustion in my 
+             |  \>  </  /    heart. Soon my prior sins may hit me. I will offer blood to my
+              \________/     weapon to save me. If one year has passed, I will be saved from the worst case.
+              _]______[_    
+              |        |     
+              |________|
+               ]______[#     
+              |        |
+              |________|     
+              _]______[_     
+              |        |     
+              |________|    
+              _]______[_
+              |        |
+              |________|
+                ]____[
+              .'      `.
+              | <   >  |>
+             <|   <   >| 
+               `.____.'
+                 V   V 
+"""""""""
 	
 #http request
 def httpcall(url):
